d6d776211bac1b4b83e66299ab0e03d6917e20bc
Reyted <2425355372@qq.com>
2024-10-22 12:38:05 +0800

    回单功能拓展

--- DayReport/dayreport.py
+++ DayReport/dayreport.py
@@ -21,8 +21,8 @@ def write_report(filepath:str,ind1:int):
     base_station=[]
     conplaint=dict()
 
-    #datestr = str((datetime.datetime.now()).strftime('%Y-%m-%d'))
-    datestr = '2024-10-19'
+    datestr = str((datetime.datetime.now()).strftime('%Y-%m-%d'))
+    #datestr = '2024-10-19'
     town_obj = {'卡若区': 0, '八宿县': 0, '边坝县': 0, '察雅县': 0, '丁青县': 0, '贡觉县': 0, '江达县': 0,
                 '类乌齐县': 0, '洛隆县': 0, '芒康县': 0, '左贡县': 0}
     town_obj_month = {'卡若区': 0, '八宿县': 0, '边坝县': 0, '察雅县': 0, '丁青县': 0, '贡觉县': 0, '江达县': 0,
@@ -166,7 +166,7 @@ def write_report(filepath:str,ind1:int):
 
 
 if __name__ == '__main__':
-    write_report('C:/Users/24253/Desktop/Python_Excel/副本昌都故障类投诉汇总20241014(2).xlsx',46)
+    write_report('C:/Users/24253/Desktop/Python_Excel/昌都故障类投诉汇总20241019(V1).xlsx',76)
 
 
 
--- Dispatch/report_test_2.py
+++ Dispatch/report_test_2.py
@@ -19,6 +19,11 @@ def creat_report(url_attach,url_tpl,url_img,city):
         pos = data[3].split('道路')[0]
         type = '弱覆盖' if data[8] == '覆盖问题' else '质差'
         datetime = data[0].strftime('%Y年%m月')
+        dt=str(data[10]).split('、')
+        dtstr=''
+        for i in dt:
+            if len(i)>2:
+                dtstr+=i.strip().replace('\n2','')
         context = {
             'system': data[2],
             'position': pos,
@@ -30,12 +35,11 @@ def creat_report(url_attach,url_tpl,url_img,city):
             'serving_cell': data[7],
             'type_define': data[11],
             'problem_analysis': data[9],
-            'solution_measures': data[10]
+            'solution_measures': data[10],
+            'solution_measures1': dtstr
         }
 
         title_lst=[data[2],pos,type,data_list[0][0].strftime('%Y年%m月'),data_list[ind][3]]
-
-
         tpl.render(context)
         tpl.save(
             f'{url_tpl}{data[2]};{pos};{data[2]}{type};{datetime}.docx')
@@ -44,13 +48,23 @@ def creat_report(url_attach,url_tpl,url_img,city):
 
     wb.close()
 
-def center_insert_img(doc, img):
+def center_insert_img(doc,img,img1):
     """插入图片"""
     for paragraph in doc.paragraphs:
-        # 根据文档中的占位符定位图片插入的位置
-        if '<<img1>>' in paragraph.text:
+        #根据文档中的占位符定位图片插入的位置
+        if 'img1' in paragraph.text:
             # 把占位符去掉
-            paragraph.text = paragraph.text.replace('<<img1>>', '')
+            paragraph.text = paragraph.text.replace('img1', '')
+            # 添加一个文字块
+            run = paragraph.add_run('')
+            # 添加一个’回车换行效果‘
+            run.add_break()
+            # 添加图片并指定大小
+            run.add_picture(img1, width=Inches(6.2))
+
+        if 'img' in paragraph.text:
+            # 把占位符去掉
+            paragraph.text = paragraph.text.replace('img', '')
             # 添加一个文字块
             run = paragraph.add_run('')
             # 添加一个’回车换行效果‘
@@ -59,14 +73,15 @@ def center_insert_img(doc, img):
             run.add_picture(img, width=Inches(6.2))
 
 
-def save_img_to_doc(img,url_tpl,title_lst:list):
+
+def save_img_to_doc(img,img1,url_tpl,title_lst:list):
     """把图片保存到doc文件中的指定位置"""
     tpl_doc = f'{url_tpl}{title_lst[0]};{title_lst[1]};{title_lst[0]}{title_lst[2]};{title_lst[3]}.docx'
     res_doc = f'{url_tpl}{title_lst[0]};{title_lst[1]};{title_lst[0]}{title_lst[2]};{title_lst[3]}.docx'
     # 打开模板文件
     document = Document(tpl_doc)
     # 插入图片居中
-    center_insert_img(document, img)
+    center_insert_img(document,img,img1)
     # 保存结果文件
     document.save(res_doc)
 
@@ -74,11 +89,12 @@ def save_img_to_doc(img,url_tpl,title_lst:list):
 def main(url_img:str,url_tpl,title_lst:list,city:str):
     """主函数"""
     img = f'{url_img}{city}/{title_lst[4]}.png'
-    save_img_to_doc(img,url_tpl,title_lst)
+    img1 = f'{url_img}{city}/{title_lst[4]}r.png'
+    save_img_to_doc(img,img1,url_tpl,title_lst)
 
 if __name__ == '__main__':
     url_attach='C:/Users/24253/Desktop/每月例行工作/10月工作内容/10月工单报告/10月工单报告附件/'
-    url_tpl='C:/Users/24253/Desktop/每月例行工作/10月工作内容/10月派单回单/派单/'
+    url_tpl='C:/Users/24253/Desktop/每月例行工作/10月工作内容/10月派单回单/回单/'
     url_img='C:/Users/24253/Desktop/每月例行工作/10月工作内容/10月派单回单/image/'
     city_lst=['昌都市城区','昌都机场高速','昌都市察雅县','昌都至察雅国道']
     for city in city_lst:
